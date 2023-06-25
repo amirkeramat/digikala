5c100dc4a6c8d3580697e3dd493f74d4d5075adb
Amirkaramat <amir_karamat@yahoo.com>
2023-06-25 20:17:23 +0330

    create urls for api

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+const MAIN_URL = "https://api.digikala.com/v1";
+
+const BASE_URL = "https://leverapi.f4rd1n.ir/api/digikala";
+
+const SINGLE_PRODUCT =
+  "https://leverapi.f4rd1n.ir/api/digikala/product/[productId]";
+
+const PRODUCT_COMMENTS =
+  "https://leverapi.f4rd1n.ir/api/digikala/product/[productId]/comments?page=[page]";
+
+const CATEGORY =
+  "https://leverapi.f4rd1n.ir/api/digikala/categories/[categoryId]/search?page=[page]";
+
+const SEARCH_PRODUCT =
+  "https://leverapi.f4rd1n.ir/api/digikala/search?q=[query]&page=[page]";
+const SEARCH_PRODUCT_BY_PRICE =
+  "https://leverapi.f4rd1n.ir/api/digikala/search?price[max]=[maxPrice]&price[min]=[minPrice]&q=[query]&page=[page]";
+
+const FILTER_BY_CATEGORY =
+  "https://leverapi.f4rd1n.ir/api/digikala/categories/[categoryId]/search?q=[query]&page=[page]";
