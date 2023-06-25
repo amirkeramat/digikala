b14aec8eb78dcdfdf9a1d0667a6c07e194e2a865
Amirkaramat <amir_karamat@yahoo.com>
2023-06-25 20:09:49 +0330

    edit productCart and fix it

--- src/components/productCart/ProductCart.jsx
+++ src/components/productCart/ProductCart.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export default function ProductCart() {
+export default function ProductCart({
+  image,
+  title,
+  prev_price,
+  current_price,
+  discount_percent,
+  is_incredible,
+}) {
   return (
-    <div>ProductCart</div>
-  )
+    <div className='product-card w-[200px] h-[300px] bg-gray-100'>
+      <div className='flex flex-col justify-evenly items-start'>
+        <img src={image} alt='' />
+        <h5 className='text-gray-950 text-xl h-[50px]'>{title}</h5>
+        <span className='flex justify-evenly items-center w-full'>
+          {is_incredible ? (
+            <span className='flex flex-col text-gray-950 items-center'>
+              <h6 className=' line-through'>
+                {prev_price}
+                {"تومان"}
+              </h6>
+              <h6>
+                {current_price}
+                {"تومان"}
+              </h6>
+            </span>
+          ) : (
+            <h6>{current_price}</h6>
+          )}
+          {discount_percent > 0 ? (
+            <h6 className='bg-red-500 text-white rounded-full px-4 flex items-center justify-center'>
+              {discount_percent}%
+            </h6>
+          ):null}
+        </span>
+      </div>
+    </div>
+  );
 }
