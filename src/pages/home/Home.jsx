795dc2ec1b3abb1062cb455910c40880acee4886
Amirkaramat <amir_karamat@yahoo.com>
2023-06-25 20:06:28 +0330

    update Home page and add the components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,29 @@
-import React, { useEffect } from 'react'
-import { fetchGetData } from '../../features/home/homeSlice'
+import { useEffect } from "react";
+import { fetchGetData } from "../../redux/slices/homeSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { Hero } from "../../components/home/hero/Hero";
+import Incredible from "../../components/home/slider/incredible/Incredible";
+import Category from "../../components/home/category/Category";
+import BestSelling from "../../components/home/slider/bestSelling/BestSelling";
+
 export default function Home() {
-  useEffect(()=>{
-    dispatch(fetchGetData("https://leverapi.f4rd1n.ir/api/digikala"));
-  },[])
-  const dispatch = useDispatch()
-  const homeStore = useSelector(state=>state.homeStore)
-  console.log(homeStore);
+  useEffect(() => {
+    dispatch(fetchGetData());
+  }, []);
+  const dispatch = useDispatch();
+  const homeState = useSelector((state) => state.homeStore);
   return (
-    <div>Home</div>
-  )
+    <main className='flex flex-col justify-center items-center'>
+      {homeState.loading === "fulfilled" ? (
+        <>
+          <Hero />
+          <Incredible />
+          <Category/>
+          <BestSelling/>
+        </>
+      ) : (
+        <div>Loading</div>
+      )}
+    </main>
+  );
 }
