32c7e38fd801204d24f69f10d87a519bea675850
Amirkaramat <amir_karamat@yahoo.com>
2023-06-25 20:06:57 +0330

    fix routes of store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./features/store.js";
+import store from "./redux/store.js";
 import "./index.css";
 import "./styles/reset.css";
 import "./styles/fonts.css";
