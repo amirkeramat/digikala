0443312dc52bddfb5cbf507b1cee999fe7a27c5f
Amirkaramat <amir_karamat@yahoo.com>
2023-06-25 20:11:04 +0330

    add subMenu component and styled component to pj

---
+++ src/components/layout/menu/MainMenu/subMenu/SubMenu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  ListItem,
+  ListTitle,
+  SubDropdown,
+  SubDropdownItems,
+  SubDropdownItem,
+  SepLine,
+  SubDropdownLink,
+} from "./subMenu.style";
+export default function SubMenu({ id, title, icon, sub }) {
+  return (
+    <ListItem>
+      {icon}
+      <ListTitle> {title}</ListTitle>
+      <SubDropdown>
+        <SubDropdownItems>
+          {sub.map((item) => (
+            <SubDropdownItem key={item.id}>
+              <SepLine />
+              <SubDropdownLink>
+                <Link to={`category/${item.code}`}>{item.title}</Link>
+              </SubDropdownLink>
+            </SubDropdownItem>
+          ))}
+        </SubDropdownItems>
+      </SubDropdown>
+    </ListItem>
+  );
+}
---
+++ src/components/layout/menu/MainMenu/subMenu/subMenu.style.js
@@ -0,0 +1,26 @@
+import tw from "tailwind-styled-components";
+
+export const ListItem = tw.li`
+    group/subMenu text-white  hover:bg-gray-50 hover:text-red-500 w-[150px] cursor-pointer text-center whitespace-nowrap py-6 flex items-center justify-evenly
+`;
+
+export const ListTitle = tw.h6`
+   text-3xl
+`;
+
+export const SubDropdown = tw.div`
+    group-hover/subMenu:block w-[550px] h-full absolute top-0 right-[150px] hidden bg-gray-50
+`;
+export const SubDropdownItems = tw.ul`
+    grid grid-cols-2 gap-10 mt-10 ms-5 text-4xl
+`;
+export const SubDropdownItem = tw.li`
+  flex items-center
+`;
+export const SepLine = tw.div`
+    bg-red-500 w-[2px] h-[30px] me-5
+`;
+export const SubDropdownLink = tw.span`
+    text-gray-800 hover:text-red-500
+`;
+
