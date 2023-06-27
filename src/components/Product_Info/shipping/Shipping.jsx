79957d1a6caf07ac9f7f0144339af902c8ba92b1
Amirkaramat <amir_karamat@yahoo.com>
2023-06-27 23:15:33 +0330

    Create shipping Component

---
+++ src/components/Product_Info/shipping/Shipping.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import useProductState from "../../../hooks/state/useProductState";
+import { FiShoppingBag } from "react-icons/fi";
+import { FaShippingFast } from "react-icons/fa";
+import {SepRow} from './Shipping.style'
+const Shipping = () => {
+  const { price } = useProductState();
+  const {
+    current_price,
+    prev_price,
+    is_incredible,
+    is_promotion,
+    discount_percent,
+  } = price;
+  return (
+    <div className="h-[500px] flex flex-col justify-evenly bg-gray-300/50 shadow-xl rounded-xl p-6">
+      <h1 className="font-bold text-3xl">فروشنده</h1>
+      <span className="flex items-center">
+        <FiShoppingBag className="text-blue-400" />
+        <p className="ms-2">بلو بی شاپ</p>
+      </span>
+      <span className="flex">
+        <p className="text-gray-400">عملکرد:</p>
+        <p className="text-green-400">عالی</p>
+      </span>
+      <SepRow />
+
+      <span className="">
+        <p className="text-gray-400">موجود در انبار فروشنده:</p>
+        <p className="text-red-400 flex items-center">
+          <FaShippingFast />
+          ارسال فوری
+        </p>
+      </span>
+      <SepRow />
+
+      <span className="">
+        {is_incredible ? (
+          <span>
+            <span className="flex items-center justify-end">
+              <p className=" line-through me-2 ">
+                {" "}
+                {prev_price.toLocaleString()}
+              </p>
+              <p className="w-[30px] h-[30px] rounded-xl bg-red-500 text-white  flex justify-center items-center">
+                {discount_percent}%
+              </p>
+            </span>
+            <span className="flex items-center justify-end">
+              <p>
+                {current_price.toLocaleString()}
+                {"تومان"}
+              </p>
+            </span>
+          </span>
+        ) : (
+          <span className="flex items-center justify-center">
+            <p>
+              {current_price.toLocaleString()}
+              {"تومان"}
+            </p>
+          </span>
+        )}
+      </span>
+      <button className="flex justify-center items-center bg-red-500 text-white w-full p-2 rounded-xl">
+        افزودن به سبد خرید
+      </button>
+    </div>
+  );
+};
+
+export default Shipping;
---
+++ src/components/Product_Info/shipping/Shipping.style.js
@@ -0,0 +1,5 @@
+import tw from "tailwind-styled-components";
+
+export const SepRow = tw.div`
+w-full h-[2px] bg-blue-600/20 my-2
+`;
