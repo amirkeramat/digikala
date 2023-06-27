a9258d283d7f629b6109cdd3b806740618d6f037
Amirkaramat <amir_karamat@yahoo.com>
2023-06-27 14:40:06 +0330

    fix productInfo.style.js

--- src/components/Product_Info/product.style.js
+++ src/components/Product_Info/product.style.js
@@ -1,7 +1,7 @@
 import tw from "tailwind-styled-components";
 
 export const Container = tw.div`
-container grid grid-cols-1 md:grid-cols-3 
+container grid grid-cols-1 md:grid-cols-3 gap-x-6
 `;
 
 export const ImgBox = tw.div`
