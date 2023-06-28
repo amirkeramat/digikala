def884a17a319157ce38af2ce5eae3f9e5bda91b
Amirkaramat <amir_karamat@yahoo.com>
2023-06-28 13:01:11 +0330

    add new Components to toot

--- src/components/Product_Info/Product.jsx
+++ src/components/Product_Info/Product.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import useProductState from "../../hooks/state/useProductState";
 import {Container} from './product.style'
 import Slider from './imageSlider/Slider'
 import Informations from "./informations/Informations";
 import Shipping from './shipping/Shipping'
 import Description from "./description/Description";
+import MoreInformation from "./moreInformation/MoreInformation";
+import RecommendProducts from "./recommendProducts/RecommendProducts";
+import Comments from "./comments/Comments";
 const Product = () => {
-  const { id ,images,mainDetails } = useProductState();
   return (
     <Container>
         <Slider/>
         <Informations/>
         <Shipping/>
         <Description/>
+        <RecommendProducts/>
+        <MoreInformation/>
+        <Comments/>
     </Container>
   );
 };
