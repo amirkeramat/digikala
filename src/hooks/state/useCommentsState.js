5e63fab54a2f7ff3aece0f19e7e7f317e505dafc
Amirkaramat <amir_karamat@yahoo.com>
2023-06-28 13:03:49 +0330

    create useCommnetsState.js

---
+++ src/hooks/state/useCommentsState.js
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+
+const useCommentsState = () => {
+  const { loading, error, comments } = useSelector((state) => state.comments);
+  return {
+    loading: loading,
+    error: error,
+    ratings: comments.ratings,
+    comments: comments.comments,
+    intentDate: comments.intent_date,
+    sortOptions: comments.sort_options,
+    mediaComments: comments.media_comments,
+    pager: {
+      currentPage: comments.pager.current_pager,
+      totalPages: comments.pager.total_pages,
+      totalItems: comments.pager.total_items,
+    },
+  };
+};
+
+export default useCommentsState;
