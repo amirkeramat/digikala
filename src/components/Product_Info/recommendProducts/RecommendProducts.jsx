eed638005c312dc3338525b164d1121c49113dc9
Amirkaramat <amir_karamat@yahoo.com>
2023-06-28 13:02:56 +0330

    create RecProduct.jsx

---
+++ src/components/Product_Info/recommendProducts/RecommendProducts.jsx
@@ -0,0 +1,13 @@
+import ProductCart from "../../common/productCart/ProductCart";
+import Slider from "../../home/slider/Slider";
+import useProductState from '../../../hooks/state/useProductState'
+const RecommendProducts = () => {
+  const {recommended} = useProductState()
+  return (
+    <div className="col-span-1 md:col-span-3">
+      <Slider data={recommended} title={"کالاهای مشابه"} />
+    </div>
+  );
+};
+
+export default RecommendProducts;
