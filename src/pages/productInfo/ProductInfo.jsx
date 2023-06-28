9b09394fdbdcf168075fc4f6803548584e36ff84
Amirkaramat <amir_karamat@yahoo.com>
2023-06-28 13:04:52 +0330

    Add PageLoader to ProductInfo.jsx

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -2,14 +2,17 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductInfo } from "../../redux/slices/productSlice";
+import {fetchComments} from '../../redux/slices/commentsSlice'
 import Product from "../../components/Product_Info/Product";
+import PageLoader from "../../components/common/pageLoader/PageLoader";
 export default function ProductInfo() {
   const { productId } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProductInfo({ id: Number(productId) }));
+    dispatch(fetchComments({ id: Number(productId) , page:1 }));
     return () => dispatch(getProductInfo());
   }, [productId]);
   const { loading } = useSelector((state) => state.singleProduct);
-  return <>{loading === "fulfilled" ? <Product /> : <div>Loading</div>}</>;
+  return <>{loading === "fulfilled" ? <Product /> : <PageLoader/>}</>;
 }
