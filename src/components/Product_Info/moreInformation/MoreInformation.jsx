6848754a183bc6647ff088d8a1b4fd7c10246181
Amirkaramat <amir_karamat@yahoo.com>
2023-06-29 19:17:17 +0330

    fix Component

--- src/components/Product_Info/moreInformation/MoreInformation.jsx
+++ src/components/Product_Info/moreInformation/MoreInformation.jsx
@@ -6,17 +6,17 @@ const MoreInformation = () => {
   const [showMore, setShowMore] = useState(false);
   return (
     <div
-      className={`col-span-1 md:col-span-3 relative transition-all duration-500 ease-in-out overflow-hidden ${
+      className={`col-span-1 md:col-span-3 relative transition-all duration-500 ease-in-out overflow-hidden shadow-xl shadow-gray-950/10 my-10 ${
         showMore ? "max-h-auto h-full" : "max-h-[220px] h-[220px]"
       }`}
     >
       <span className="flex">
-        <h1 className="p-2 flex justify-center rounded-2xl font-bold text-4xl bg-gray-950/10 w-[200px]">
+        <h1 className="p-2 flex justify-center  font-bold text-4xl w-[200px]">
           مشخصات بیشتر
         </h1>
         <button
           onClick={() => setShowMore((prv) => !prv)}
-          className="flex justify-center items-center bg-blue-500/10 w-[50px] text-4xl text-blue-500 rounded-full"
+          className="flex justify-center items-center bg-blue-500/10 w-[50px] text-4xl text-blue-500 rounded-xl"
         >
           {showMore ? <FaArrowUp /> : <FaArrowDown />}
         </button>
@@ -24,10 +24,13 @@ const MoreInformation = () => {
 
       {information !== null && (
         <>
-          {information.map((item) => {
+          {information.map((item,index) => {
             const { title, values } = item;
             return (
-              <span className="flex w-[full] border border-gray-950/10 p-2">
+              <span
+                key={index}
+                className="flex w-[full] border border-gray-950/10 p-2"
+              >
                 <p className="mx-4 text-gray-950/80">{title}:</p>
                 <p className="font-semibold">{values}</p>
               </span>
