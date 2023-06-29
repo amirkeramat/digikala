755ba98a8ecae5baa043311761cb3819f33e3d67
Amirkaramat <amir_karamat@yahoo.com>
2023-06-29 19:14:29 +0330

    fix page loader

--- src/components/common/pageLoader/PageLoader.jsx
+++ src/components/common/pageLoader/PageLoader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ImageLoader from "../../../assets/imageLoader.svg";
 const PageLoader = () => {
   return (
-    <div className="w-full absolute top-[150px] inset-0 flex justify-center items-center overflow-y-hidden bg-gray-950/10">
+    <div className="w-screen h-screen  sticky top-[150px] inset-0 flex justify-center items-center overflow-y-hidden bg-gray-950/10">
       <div className="flex justify-center border border-yellow-400 shadow-2xl bg-white rounded-3xl">
         <img className="w-[200px]" src="/light-logo.png" />
         <img className=" h-[100px]" src={ImageLoader} />
