ff0cd9f90eebb4d81020018766510bff98292367
Amirkaramat <amir_karamat@yahoo.com>
2023-06-29 19:18:59 +0330

    Add categorySore to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import homeState from "./slices/homeSlice";
 import singleProductReducer from "./slices/productSlice";
 import commentState from "./slices/commentsSlice";
+import categoriesState from './slices/categorySlice'
 const store = configureStore({
   reducer: {
     homeStore: homeState,
     singleProduct: singleProductReducer,
     comments: commentState,
+    categoriesStore: categoriesState,
   },
 });
 
