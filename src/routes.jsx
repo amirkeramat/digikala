04474ccb7e8f0360a5139b502cac9fe71eb06510
Amirkaramat <amir_karamat@yahoo.com>
2023-06-29 19:19:13 +0330

    Add categoires route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,12 @@
 import Home from './pages/home/Home'
 import AllProducts from "./pages/allProducts/AllProducts";
 import ProductInfo from "./pages/productInfo/ProductInfo";
-
+import Categories from './pages/categories/Categories';
 const routes = [
   { path: "/", element: <Home /> },
   { path: "products", element: <AllProducts /> },
-  { path: "productInfo/:productName/:productId", element: <ProductInfo /> },
-  // { path: "category/:categoryName", element: <ProductInfo /> },
+  { path: "productInfo/:productId", element: <ProductInfo /> },
+  { path: "category/:categoryName/:pageNumber", element: <Categories /> },
   // { path: "checkout/cart", element: <ProductInfo /> },
 ];
 
