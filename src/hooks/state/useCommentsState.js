d87cd4450c954cbaf5d68132b63eca84ff168373
Amirkaramat <amir_karamat@yahoo.com>
2023-07-01 15:57:39 +0330

    fix useCommentsState

--- src/hooks/state/useCommentsState.js
+++ src/hooks/state/useCommentsState.js
@@ -1,20 +1,22 @@
 import { useSelector } from "react-redux";
 
 const useCommentsState = () => {
-  const { loading, error, comments } = useSelector((state) => state.comments);
+  const { loading, error, comments ,pager } = useSelector((state) => state.comments);
   return {
     loading: loading,
     error: error,
-    ratings: comments.ratings,
-    comments: comments.comments,
-    intentDate: comments.intent_date,
-    sortOptions: comments.sort_options,
-    mediaComments: comments.media_comments,
-    pager: {
-      currentPage: comments.pager.current_page,
-      totalPages: comments.pager.total_pages,
-      totalItems: comments.pager.total_items,
-    },
+    comments:comments,
+    pager:pager
+    // ratings: comments.ratings,
+    // comments: comments.comments,
+    // intentDate: comments.intent_date,
+    // sortOptions: comments.sort_options,
+    // mediaComments: comments.media_comments,
+    // pager: {
+    //   currentPage: comments.pager.current_page,
+    //   totalPages: comments.pager.total_pages,
+    //   totalItems: comments.pager.total_items,
+    // },
   };
 };
 
