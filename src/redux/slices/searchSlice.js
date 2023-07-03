ed4507e3c21740e941f63f2f24a52bcc0f642387
Amirkaramat <amir_karamat@yahoo.com>
2023-07-03 16:22:43 +0330

    create searchSlice

---
+++ src/redux/slices/searchSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { SEARCH_PRODUCT } from "../../api/index";
+
+const initialState = {
+  loading: "",
+  error: "",
+  data: {},
+};
+export const searchData = createAsyncThunk("search/searchData", async (arg) => {
+  const { name, page } = arg;
+  return await axios
+    .get(`${SEARCH_PRODUCT}q=${name}page=${page}`)
+    .then((Response) => Response.data.results)
+    .catch((error) => error.message);
+});
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(searchData.pending, (state) => {
+      state.loading = "pending";
+    });
+    builder.addCase(searchData.fulfilled, (state, action) => {
+      (state.loading = "fulfilled"), (state.data = action.payload);
+    });
+    builder.addCase(searchData.rejected, (state, action) => {
+      (state.loading = "rejected"), (state.error = action.payload.error);
+    });
+  },
+});
+
+export default searchSlice.reducer;
