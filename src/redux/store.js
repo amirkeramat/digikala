6625814c55127b54a7469074ad4036efe0873890
Amirkaramat <amir_karamat@yahoo.com>
2023-07-03 16:22:29 +0330

    add searchReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import homeState from "./slices/homeSlice";
 import singleProductReducer from "./slices/productSlice";
 import commentState from "./slices/commentsSlice";
-import categoriesState from './slices/categorySlice'
+import categoriesState from "./slices/categorySlice";
+import searchReducer from "./slices/searchSlice";
 const store = configureStore({
   reducer: {
     homeStore: homeState,
     singleProduct: singleProductReducer,
     comments: commentState,
     categoriesStore: categoriesState,
+    searchStore: searchReducer,
   },
+  // devTools: false,
 });
 
 export default store;
