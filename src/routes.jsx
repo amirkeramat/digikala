1feca54aa8a9e3917aef6588c851f2ae29d73948
Amirkaramat <amir_karamat@yahoo.com>
2023-07-03 16:22:06 +0330

    add search Route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
-import Home from './pages/home/Home'
+import Home from "./pages/home/Home";
 import ProductInfo from "./pages/productInfo/ProductInfo";
-import Categories from './pages/categories/Categories';
+import Categories from "./pages/categories/Categories";
+import Search from "./pages/search/Search";
 const routes = [
   { path: "/", element: <Home /> },
   { path: "productInfo/:productId", element: <ProductInfo /> },
   { path: "category/:categoryName", element: <Categories /> },
+  { path: "search/:productName/:pageNumber", element: <Search /> },
   // { path: "checkout/cart", element: <ProductInfo /> },
 ];
 
