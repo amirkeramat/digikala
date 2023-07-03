0c88aa90008258c69a026d6ba93e137423c317b9
Amirkaramat <amir_karamat@yahoo.com>
2023-07-03 16:26:21 +0330

    replace searchBar name by searchForm

---
+++ src/components/layout/header/SearchForm.jsx
@@ -0,0 +1,35 @@
+import {
+  Form,
+  SearchFormContainer,
+  SearchInput,
+  SearchButton,
+} from "./header.style";
+import { FiSearch } from "react-icons/fi";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+const SearchForm = () => {
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
+  const searchHandler = (data) => {
+    if (data.searchValue.trim().length) {
+      navigate(`/search/${data.searchValue.trim()}/1`);
+      reset();
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit(searchHandler)}>
+      <SearchFormContainer>
+        <SearchInput
+          {...register("searchValue")}
+          type="text"
+          placeholder="جستجو"
+        />
+        <SearchButton type="submit">
+          <FiSearch />
+        </SearchButton>
+      </SearchFormContainer>
+    </Form>
+  );
+};
+
+export default SearchForm;
