e13b2c27cc671e964100e5131227996d562e9609
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 19:00:41 +0330

    fix image

--- src/components/common/pageLoader/PageLoader.jsx
+++ src/components/common/pageLoader/PageLoader.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ImageLoader from "./imageLoader.svg";
+import ImageLoader from "../../../../public/imageLoader.svg";
 import { Container, LoaderBox } from "./pageLoader.style";
 const PageLoader = () => {
   return (
     <Container>
       <LoaderBox>
-        <img className="w-[200px]" src="./light-logo.png" />
-        <img className=" h-[100px]" src={ImageLoader} />
+        <img className='w-[200px]' src='./light-logo.png' />
+        <img className=' h-[100px]' src={ImageLoader} />
       </LoaderBox>
     </Container>
   );
