54bb8f31affcafa2356f0ad843398a128a40995f
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 18:47:33 +0330

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import "./index.css";
@@ -10,9 +10,9 @@ import "./styles/fonts.css";
 import "./styles/defaults.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
