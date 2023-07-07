6f892c5485c8903fe30e14ac843f75aa6ea8a583
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 19:02:34 +0330

    fix image

--- src/components/common/imageLoader/ImageLoader.jsx
+++ src/components/common/imageLoader/ImageLoader.jsx
@@ -1,4 +1,4 @@
-import Loader from "../../../assets/imageLoader.svg";
+import Loader from "../../../../public/imageLoader.svg";
 import { Image } from "./imageLoader.style";
 const ImageLoader = () => {
   return <Image src={Loader} />;
