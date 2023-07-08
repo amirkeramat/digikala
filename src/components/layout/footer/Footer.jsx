e2716d6d12a0d2537f0b1317590c8dd6135f4738
Amirkaramat <amir_karamat@yahoo.com>
2023-07-08 13:41:28 +0330

    Update Footer.jsx

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   const { icons, links, social } = footerItems;
   return (
-    <div className="container mt-10 p-2">
+    <div className="container mt-10 py-2 px-4 flex flex-col justify-center w-full h-full">
       <span className="flex justify-between items-center">
-        <img src="/light-logo.png" className="w-[100px]" alt="" />
+        <Link to={"/"}>
+          <img src="/light-logo.png" className="w-[100px]" alt="" />
+        </Link>
         <button
           onClick={() =>
             window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
@@ -23,28 +25,33 @@ export default function Footer() {
       </span>
       <div className="grid grid-cols-2 gap-6 md:grid-cols-5 flex-wrap mt-5">
         {icons.map((icon) => (
-          <span key={icon.id} className="flex flex-col text-xl items-center justify-center ">
+          <span
+            key={icon.id}
+            className="flex flex-col text-xl items-center justify-center "
+          >
             <img className="w-[50px]" src={icon.img} alt="" />
             <p className=" whitespace-nowrap">{icon.title}</p>
           </span>
         ))}
       </div>
-      <div className="grid mt-5 grid-cols-2 md:grid-cols-4 p-2 ">
+      <div className="grid mt-5 grid-cols-2 md:grid-cols-4 px-2 py-10 ">
         {links.map((link) => (
-          <u key={link.id}l>
+          <ul key={link.id} l>
             <li className="font-semibold">{link.title}</li>
             <li className="flex flex-col">
               {link.subLinks.map((subLink) => (
                 <Link key={subLink.id}>{subLink.title}</Link>
               ))}
             </li>
-          </u>
+          </ul>
         ))}
         <span>
-          <h6>{social.title}</h6>
+          <h6 className="font-semibold">{social.title}</h6>
           <span className="flex justify-start">
             {social.icons.map((icon) => (
-              <i key={icon.id} className="text-4xl me-4 py-4">{icon.icon}</i>
+              <i key={icon.id} className="text-4xl me-4 py-4">
+                {icon.icon}
+              </i>
             ))}
           </span>
         </span>
