c6a03c212045d91c54309ab80b883bb448a64af5
Amirkaramat <amir_karamat@yahoo.com>
2023-07-08 13:42:36 +0330

    Update MoreInformation.jsx

--- src/components/Product_Info/moreInformation/MoreInformation.jsx
+++ src/components/Product_Info/moreInformation/MoreInformation.jsx
@@ -6,17 +6,17 @@ const MoreInformation = () => {
   const [showMore, setShowMore] = useState(false);
   return (
     <div
-      className={`col-span-1 md:col-span-3 relative transition-all duration-500 ease-in-out overflow-hidden shadow-xl shadow-gray-950/10 my-10 p-4 ${
+      className={`col-span-1 md:col-span-3 relative transition-all duration-500 ease-in-out overflow-hidden shadow-sm shadow-gray-950/10 my-10 p-4 ${
         showMore ? "max-h-auto h-full" : "max-h-[220px] h-[220px]"
       }`}
     >
-      <span className="flex">
+      <span className="flex items-center">
         <h1 className="p-2 flex justify-center  font-bold text-4xl w-[200px]">
           مشخصات بیشتر
         </h1>
         <button
           onClick={() => setShowMore((prv) => !prv)}
-          className="flex justify-center items-center bg-blue-500/10 w-[50px] text-4xl text-blue-500 rounded-xl"
+          className="flex justify-center items-center bg-blue-500/10 w-[30px] h-[30px] text-2xl text-blue-500 rounded-xl"
         >
           {showMore ? <FaArrowUp /> : <FaArrowDown />}
         </button>
@@ -24,7 +24,7 @@ const MoreInformation = () => {
 
       {information !== null && (
         <>
-          {information.map((item,index) => {
+          {information.map((item, index) => {
             const { title, values } = item;
             return (
               <span
@@ -40,7 +40,7 @@ const MoreInformation = () => {
       )}
       <button
         onClick={() => setShowMore((prv) => !prv)}
-        className="flex justify-center items-center bg-blue-500/10 w-[50px] text-4xl text-blue-500 rounded-full"
+        className="flex justify-center items-center bg-blue-500/10 w-[30px] h-[30px] text-2xl text-blue-500 rounded-xl"
       >
         {showMore ? <FaArrowUp /> : <FaArrowDown />}
       </button>
