c1ff3af1975425059f3822523d2fb5a60e6738b3
Amirkaramat <amir_karamat@yahoo.com>
2023-07-08 13:43:39 +0330

    Update roductInfo.jsx

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -5,14 +5,20 @@ import { getProductInfo } from "../../redux/slices/productSlice";
 import { fetchComments } from "../../redux/slices/commentsSlice";
 import Product from "../../components/Product_Info/Product";
 import PageLoader from "../../components/common/pageLoader/PageLoader";
+import ErrorPage from "../../components/common/errorPage/ErrorPage";
 export default function ProductInfo() {
   const { productId } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProductInfo({ id: Number(productId) }));
     dispatch(fetchComments({ id: Number(productId), page: 1 }));
-    
   }, [productId]);
-  const { loading ,data } = useSelector((state) => state.singleProduct);
-  return <>{loading === "fulfilled"? <Product /> : <PageLoader />}</>;
+  const { loading, data } = useSelector((state) => state.singleProduct);
+  if (loading === "loading") {
+    return <PageLoader />;
+  } else if (loading === "fulfilled" && Object.keys(data).length) {
+    return <Product />;
+  } else if (loading === "fulfilled" && !Object.keys(data).length) {
+    return <ErrorPage />;
+  }
 }
