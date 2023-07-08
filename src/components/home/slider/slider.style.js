499c819e1b7520f062b5b77ad9c2695992c357a4
Amirkaramat <amir_karamat@yahoo.com>
2023-07-08 13:41:18 +0330

    Update slider.style.js

--- src/components/home/slider/slider.style.js
+++ src/components/home/slider/slider.style.js
@@ -1,8 +1,8 @@
 import tw from "tailwind-styled-components"
 
 export const Container = tw.section`
-container mt-10 border border-gray-950/10 rounded-xl shadow-xl 
-shadow-gray-950/10 
+container mt-10 border border-gray-950/10 rounded-xl shadow-sm 
+shadow-gray-950/20 
 `;
 
 export const Title = tw.h1`
